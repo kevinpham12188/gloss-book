eb7edc3a76d4c576c507a7467273201f0e372a9b
Kevin Pham <kevinpham7788@gmail.com>
2026-03-25 10:09:11 -0700

    feat: add BookingDbContext with EF Core configuration

---
+++ booking-service/GlossBook.BookingService/Data/BookingDbContext.cs
@@ -0,0 +1,28 @@
+using GlossBook.BookingService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlossBook.BookingService.Data
+{
+    public class BookingDbContext : DbContext
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Appointment> Appointments{ get; set; }
+        public DbSet<StaffBreak> StaffBreaks { get; set; }
+        public DbSet<StaffSchedule> StaffSchedules { get; set; }
+        public DbSet<StaffScheduleOverride> StaffScheduleOverrides { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Appointment>(entity =>
+                {
+                    entity.Property(a => a.Status).HasConversion<string>();
+                    entity.Property(a => a.PriceAtBooking).HasColumnType("numeric(10,2)");
+                });
+            }
+    }
+}
