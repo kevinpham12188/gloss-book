1e9daafa43465eb4cc4b6ecf30391327815c1283
Kevin Pham <kevinpham7788@gmail.com>
2026-03-26 10:10:53 -0700

    refactor: move ApiResponse to DTOs/Responses and update Program.cs registrations

--- booking-service/GlossBook.BookingService/Program.cs
+++ booking-service/GlossBook.BookingService/Program.cs
@@ -1,14 +1,21 @@
 using GlossBook.BookingService.Data;
+using GlossBook.BookingService.Repositories;
+using GlossBook.BookingService.Repositories.Interfaces;
+using GlossBook.BookingService.Services;
+using GlossBook.BookingService.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BookingDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    .LogTo(Console.WriteLine, LogLevel.Information);
 });
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
 var app = builder.Build();
 
